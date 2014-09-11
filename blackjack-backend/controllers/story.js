571681cb35053f53fdb98d3df73ab599bdba34c8
alexzad <alexzad@gmail.com>
2014-09-11 22:44:30 +0000

    fixes

--- blackjack-backend/controllers/story.js
+++ blackjack-backend/controllers/story.js
@@ -43,6 +43,49 @@ exports.init = function(app, mongoose) {
             }
         });
     });
+    app.put("(/api)?/stories/:0", function(req, res) {
+        console.log("create story: " + JSON.stringify(req.body));
+        var id = req.params[0];
+        var query = Story.where({
+            _id: id
+        });
+        var newVal = {};
+        if(req.body.estimate) {
+            newVal.estimate = req.body.estimate;
+        }
+        if(req.body.active) {
+            newVal.active = req.body.active;
+        }
+        if(req.body.revealed) {
+            newVal.revealed = req.body.revealed;
+        }
+        query.findOne(function(err, story) {
+            if(err) {
+                handleError(req, res, err);
+            } else if(desk == null) {
+                console.log("story " + id + " not found");
+                res.status(404).send({
+                    errorCode: 404,
+                    errorMessage: "story " + id + " not found"
+                });
+            } else {
+                console.log('found: ' + JSON.stringify(story));
+                Story.update({
+                    _id: id
+                }, newVal, function(err) {
+                    if(err) {
+                        handleError(req, res, err);
+                    } else {
+                        console.log("story id: " + id + " updated with new values: " + JSON.stringify(newVal));
+                        res.set("Content-type", "application/json");
+                        res.status(200).send({
+                            storyId: id
+                        });
+                    }
+                });
+            }
+        });
+    });
 
     function handleError(req, res, err) {
         console.log("error: " + JSON.stringify(err));
