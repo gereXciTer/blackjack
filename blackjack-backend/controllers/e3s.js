68ad4768c551ed94fa8e32429821d328281b173a
alexzad <alexzad@gmail.com>
2014-09-18 15:24:11 +0000

    adding timestamp to log output

--- blackjack-backend/controllers/e3s.js
+++ blackjack-backend/controllers/e3s.js
@@ -21,17 +21,6 @@ exports.init = function(app, mongoose) {
                 return item.data;
             }));
         });
-        /*
-        e3s.proxyE3SReq(req, res, '/rest/e3s-eco-scripting-impl/0.1.0/data/searchFts?type=com.epam.e3s.app.project.api.data.ProjectProjectionEntity&query={%22statements%22:colon[{%22query%22:colon2%22name:colon3(:query)%22}]}&fields=teamUpsaIds', {
-            query: req.query.name
-        }, function(body) {
-            console.log("received project list");
-            var items = JSON.parse(body)[0].data.teamUpsaIds;
-            e3s.proxyE3SReq(req, res, '/rest/e3s-eco-scripting-impl/0.1.0/data/select?type=com.epam.e3s.app.people.api.data.EmployeeEntity&query={%22upsaidSum%22:colon{%22$in%22:colon1:teamUpsaIds}}&fields=photosSum,fullNameSum,upsaidSum,emailSum&limit=100', {
-                teamUpsaIds: JSON.stringify(items)
-            });
-        });
-        */
     });
     console.log("e3s controller initialized");
 };
